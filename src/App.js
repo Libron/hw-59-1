631612a14c32c8ea1b53c2cb8e997181e9a10569
Urmat Bekboev <bek.urmat@gmail.com>
2019-01-18 06:33:02 +0600

    Rendering all components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Form from "./components/Form/Form";
+import Movies from "./components/Movies/Movies";
 
 class App extends Component {
+    state = {
+      movies: [
+          {title: 'Watch Dog', id: 123},
+          {title: 'Termintar', id: 245},
+          {title: 'Ubit billa', id: 255},
+          {title: 'spongeBobo', id: 566}
+      ]
+    };
+
+    addMovie = event => {
+        event.preventDefault();
+        console.log('Hello');
+    };
+
+    changeMovieTitle = (event, id) => {
+      const movies = [...this.state.movies];
+      const targetIndex = movies.findIndex(movie => movie.id === id);
+      const targetMovie = {...movies[targetIndex]};
+      targetMovie.title = event.target.value;
+      movies[targetIndex] = targetMovie;
+      this.setState({movies});
+    };
+
+    removeMovie = id => {
+        const movies = [...this.state.movies];
+        const targetIndex = movies.findIndex(movie => movie.id === id);
+        movies.splice(targetIndex, 1);
+        this.setState({movies});
+    };
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Form formSubmitted={event => this.addMovie(event)}/>
+          <Movies
+              list={this.state.movies}
+              changeTitle={this.changeMovieTitle}
+              removeMovie={this.removeMovie}
+          />
       </div>
     );
   }
