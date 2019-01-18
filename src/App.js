2fd4c9c19fec70ff5705ad37702a694b5c56fe01
Urmat Bekboev <bek.urmat@gmail.com>
2019-01-18 15:48:22 +0600

    Added new feature "Save to LocalStorage"

--- src/App.js
+++ src/App.js
@@ -4,33 +4,44 @@ import Form from "./components/Form/Form";
 import Movies from "./components/Movies/Movies";
 
 class App extends Component {
+    static dbName = 'MovieDB ';
+
     state = {
-      movies: [
-          {title: 'Watch Dog', id: 123},
-          {title: 'Termintar', id: 245},
-          {title: 'Ubit billa', id: 255},
-          {title: 'spongeBobo', id: 566}
-      ]
+      movies: []
+    };
+
+    constructor(props) {
+        super(props);
+        this.storage = this.initStorage();
+    };
+
+    componentDidMount() {
+        this.setState({movies: this.storage});
+    };
+
+    componentDidUpdate() {
+      this.updateStorage();
     };
 
     addMovieHandler = event => {
         event.preventDefault();
-        const movies = [...this.state.movies];
+
         const userInput = document.getElementById('title').value;
-        const uniqueID = this.getUniqueID();
-        movies.push({title: userInput, id: uniqueID});
-        this.setState({movies});
+        if (userInput) {
+            const movies = [...this.state.movies];
+            const uniqueID = this.getUniqueID();
+            movies.push({title: userInput, id: uniqueID});
+            this.setState({movies});
+        }
     };
 
-    getUniqueID = () => (Math.floor(Date.now() / 1000) + Math.floor(Math.random() * 101));
-
     changeMovieTitle = (event, id) => {
-      const movies = [...this.state.movies];
-      const targetIndex = movies.findIndex(movie => movie.id === id);
-      const targetMovie = {...movies[targetIndex]};
-      targetMovie.title = event.target.value;
-      movies[targetIndex] = targetMovie;
-      this.setState({movies});
+        const movies = [...this.state.movies];
+        const targetIndex = movies.findIndex(movie => movie.id === id);
+        const targetMovie = {...movies[targetIndex]};
+        targetMovie.title = event.target.value;
+        movies[targetIndex] = targetMovie;
+        this.setState({movies});
     };
 
     removeMovie = id => {
@@ -38,6 +49,23 @@ class App extends Component {
         const targetIndex = movies.findIndex(movie => movie.id === id);
         movies.splice(targetIndex, 1);
         this.setState({movies});
+    };
+
+    getUniqueID = () => (Math.floor(Date.now() / 1000) + Math.floor(Math.random() * 101));
+
+    initStorage = () => {
+        if (localStorage.getItem(App.dbName) === null) {
+            localStorage.setItem(App.dbName, '[]');
+            return [];
+        } else {
+            const data = localStorage.getItem(App.dbName);
+            return JSON.parse(data);
+        }
+    };
+
+    updateStorage = () => {
+        let movies = this.state.movies;
+        localStorage.setItem(App.dbName, JSON.stringify(movies));
     };
 
   render() {
